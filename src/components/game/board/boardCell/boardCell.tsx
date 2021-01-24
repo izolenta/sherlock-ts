90f23c81c52216cc9a441906d7bd37e80eb2b069
Alexey Sharov <lehtenstaarn@gmail.com>
2021-01-25 00:50:22 +0100

    brd!

--- src/components/game/board/boardCell/boardCell.tsx
+++ src/components/game/board/boardCell/boardCell.tsx
@@ -2,6 +2,7 @@ import React, { Dispatch } from "react";
 import { CellState } from "../../../../models/cellState";
 import { TAction } from "../../../../store/actions";
 import './boardCell.css';
+import '../../../../styles/common.css'
 
 interface BoardCellProps {
   state: CellState,
@@ -11,6 +12,11 @@ interface BoardCellProps {
 export default class BoardCell extends React.Component<BoardCellProps> {
 
   render() {
-    return <div className='cell'></div>
+    let line = Math.floor(this.props.state.index / 6);
+    let position = this.props.state.properSolution;
+    let myClass='sprite large resolved s'+ line + position;
+    return <div className='cell'>
+      <div className={myClass}></div>
+    </div>
   }
 }
--- src/store/initialState.ts
+++ src/store/initialState.ts
@@ -1,10 +1,10 @@
 import {SherlockState} from '../models/sherlockState'
 import {initRandomConfiguration} from "../service/gameService";
-import {DIFFICULTY_EASY, DIFFICULTY_HARD} from "../models/gameDifficulties.d";
+import {DIFFICULTY_EASY} from "../models/gameDifficulties.d";
 
 const initialState: SherlockState = {
   isRulesOn: true,
-  gameState: initRandomConfiguration(DIFFICULTY_HARD),
+  gameState: initRandomConfiguration(DIFFICULTY_EASY),
 } 
 
 export default initialState;
