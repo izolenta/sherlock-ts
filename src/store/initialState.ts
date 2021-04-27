b2566e613ce09af3a8050fd978d62da4706c660d
Alexey Sharov <lehtenstaarn@gmail.com>
2021-04-27 21:36:12 +0200

    feature-parity

--- src/store/initialState.ts
+++ src/store/initialState.ts
@@ -1,6 +1,6 @@
 import {SherlockState} from '../models/sherlockState'
 import {initRandomConfiguration} from "../service/gameService";
-import {DIFFICULTY_EASY} from "../models/gameDifficulties";
+import {DIFFICULTY_EASY} from "../models/gameDifficulties.d";
 
 const initialState: SherlockState = {
   isRulesOn: true,
